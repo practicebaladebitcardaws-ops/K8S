80ecb052a39fb37b9b417a5a6d09be30f489160c
root <root@ip-172-31-46-245.ap-southeast-2.compute.internal>
2026-02-10 16:18:19 +0000

    K8S files from the server

---
+++ K8S/kubernee.sh
@@ -0,0 +1,75 @@
+#!/bim/bash
+
+sudo apt update
+
+# install containerd using the apt package manager
+# containerd is lightwieght, reliable and fast (CRI native)
+sudo apt-get install -y containerd
+
+# create /etc/containerd directory for containerd configuration
+sudo mkdir -p /etc/containerd
+
+# Generate the default containerd configuration
+# Change the pause container to version 3.10 (pause container holds the linux ns for Kubernetes namespaces)
+# Set `SystemdCgroup` to true to use same cgroup drive as kubelet
+containerd config default \
+| sed 's/SystemdCgroup = false/SystemdCgroup = true/' \
+| sed 's|sandbox_image = ".*"|sandbox_image = "registry.k8s.io/pause:3.10"|' \
+| sudo tee /etc/containerd/config.toml > /dev/null
+
+# Restart containerd to apply the configuration changes
+sudo systemctl restart containerd
+
+# Kubernetes doesn’t support swap unless explicitly configured under cgroup v2
+sudo swapoff -a
+
+sudo apt update
+
+# install apt-transport-https ca-certificates curl and gpg packages using 
+# apt package manager in order to fetch Kubernetes packages from 
+# external HTTPS repositories
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+# create a secure directory for storing GPG keyring files 
+# used by APT to verify trusted repositories. 
+# This is part of a newer, more secure APT repository layout that 
+# keeps trusted keys isolated from system-wide GPG configurations
+sudo mkdir -p -m 755 /etc/apt/keyrings
+
+# download the k8s release gpg key FOR 1.33
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+
+# Download and convert the Kubernetes APT repository's GPG public key into
+# a binary format (`.gpg`) that APT can use to verify the integrity
+# and authenticity of Kubernetes packages during installation. 
+# This overwrites any existing configuration in 
+# /etc/apt/sources.list.d/kubernetes.list FOR 1.33 
+# (`tee` without `-a` (append) will **replace** the contents of the file)
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# update packages in apt 
+sudo apt-get update
+
+apt-cache madison kubelet
+apt-cache madison kubectl
+apt-cache madison kubeadm
+
+
+KUBE_VERSION="1.33.2-1.1"
+
+# install kubelet, kubeadm, and kubectl at version 1.33.2-1.1
+sudo apt-get install -y kubelet=$KUBE_VERSION kubeadm=$KUBE_VERSION kubectl=$KUBE_VERSION
+
+# hold these packages at version 
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# uncomment the line in /etc/sysctl.conf enabling IP forwarding after reboot
+sudo sed -i '/^#net\.ipv4\.ip_forward=1/s/^#//' /etc/sysctl.conf
+
+# Apply the changes to sysctl.conf
+# Any changes made to sysctl configuration files take immediate effect without requiring a reboot
+sudo sysctl -p
+
